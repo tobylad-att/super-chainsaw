562d93a5d1f2b95d9ae114a9e64489ff10da0c5f
Ladislas, Toby (tl5984) <tl5984@att.com>
2022-11-03 16:20:52 -0400

    Adds build to git

---
+++ build/precache-manifest.c27f5873b03ad97de57b288615dd3df6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080d7e1eab3137612ce9675acd420ce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17650d2fb97c94b69bbc",
+    "url": "/static/css/main.b170c974.chunk.css"
+  },
+  {
+    "revision": "390325054fb985780f9f",
+    "url": "/static/js/2.04751ee6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.04751ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17650d2fb97c94b69bbc",
+    "url": "/static/js/main.cba70ce5.chunk.js"
+  },
+  {
+    "revision": "c63e73a1558be330fb05",
+    "url": "/static/js/runtime-main.ab52df60.js"
+  }
+]);
---
+++ build/static/js/main.cba70ce5.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsuper-chainsaw"]=this["webpackJsonpsuper-chainsaw"]||[]).push([[0],{12:function(e,t,n){},13:function(e,t,n){"use strict";n.r(t);var c=n(0),r=n.n(c),a=n(2);n(12);var o=function(){return r.a.createElement("div",{className:"hello"},"Hello ","Cricket")};Object(a.createRoot)(document.getElementById("root")).render(r.a.createElement(o,null))},3:function(e,t,n){e.exports=n(13)}},[[3,1,2]]]);
+//# sourceMappingURL=main.cba70ce5.chunk.js.map
